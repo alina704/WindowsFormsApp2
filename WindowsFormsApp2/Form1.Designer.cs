8df95b1c838b13f2394d18984f2d374a295e4d89
Alina <alinaabraskina73@gmail.com>
2020-03-17 09:31:27 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.one = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.chree = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.seven = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.umnogenie = new System.Windows.Forms.Button();
+            this.nul = new System.Windows.Forms.Button();
+            this.ravno = new System.Windows.Forms.Button();
+            this.delenie = new System.Windows.Forms.Button();
+            this.ochstit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb
+            // 
+            this.tb.Location = new System.Drawing.Point(13, 13);
+            this.tb.Multiline = true;
+            this.tb.Name = "tb";
+            this.tb.Size = new System.Drawing.Size(245, 29);
+            this.tb.TabIndex = 0;
+            this.tb.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(268, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // one
+            // 
+            this.one.BackColor = System.Drawing.SystemColors.Window;
+            this.one.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.one.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.one.Location = new System.Drawing.Point(16, 61);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(45, 32);
+            this.one.TabIndex = 2;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = false;
+            this.one.Click += new System.EventHandler(this.one_Click);
+            // 
+            // two
+            // 
+            this.two.BackColor = System.Drawing.SystemColors.Window;
+            this.two.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.two.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.two.Location = new System.Drawing.Point(76, 61);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(45, 32);
+            this.two.TabIndex = 3;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = false;
+            this.two.Click += new System.EventHandler(this.two_Click);
+            // 
+            // chree
+            // 
+            this.chree.BackColor = System.Drawing.SystemColors.Window;
+            this.chree.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.chree.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chree.Location = new System.Drawing.Point(139, 61);
+            this.chree.Name = "chree";
+            this.chree.Size = new System.Drawing.Size(45, 32);
+            this.chree.TabIndex = 4;
+            this.chree.Text = "3";
+            this.chree.UseVisualStyleBackColor = false;
+            this.chree.Click += new System.EventHandler(this.chree_Click);
+            // 
+            // plus
+            // 
+            this.plus.BackColor = System.Drawing.SystemColors.Window;
+            this.plus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.plus.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.plus.Location = new System.Drawing.Point(202, 61);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(45, 32);
+            this.plus.TabIndex = 5;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = false;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // four
+            // 
+            this.four.BackColor = System.Drawing.SystemColors.Window;
+            this.four.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.four.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.four.Location = new System.Drawing.Point(16, 114);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(45, 32);
+            this.four.TabIndex = 6;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = false;
+            this.four.Click += new System.EventHandler(this.four_Click);
+            // 
+            // five
+            // 
+            this.five.BackColor = System.Drawing.SystemColors.Window;
+            this.five.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.five.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.five.Location = new System.Drawing.Point(76, 114);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(45, 32);
+            this.five.TabIndex = 7;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = false;
+            this.five.Click += new System.EventHandler(this.five_Click);
+            // 
+            // six
+            // 
+            this.six.BackColor = System.Drawing.SystemColors.Window;
+            this.six.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.six.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.six.Location = new System.Drawing.Point(139, 114);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(45, 32);
+            this.six.TabIndex = 8;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = false;
+            this.six.Click += new System.EventHandler(this.six_Click);
+            // 
+            // minus
+            // 
+            this.minus.BackColor = System.Drawing.SystemColors.Window;
+            this.minus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minus.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.minus.Location = new System.Drawing.Point(202, 114);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(45, 32);
+            this.minus.TabIndex = 9;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = false;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // seven
+            // 
+            this.seven.BackColor = System.Drawing.SystemColors.Window;
+            this.seven.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.seven.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.seven.Location = new System.Drawing.Point(16, 163);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(45, 32);
+            this.seven.TabIndex = 10;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = false;
+            this.seven.Click += new System.EventHandler(this.seven_Click);
+            // 
+            // eight
+            // 
+            this.eight.BackColor = System.Drawing.SystemColors.Window;
+            this.eight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.eight.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.eight.Location = new System.Drawing.Point(76, 163);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(45, 32);
+            this.eight.TabIndex = 11;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = false;
+            this.eight.Click += new System.EventHandler(this.eight_Click);
+            // 
+            // nine
+            // 
+            this.nine.BackColor = System.Drawing.SystemColors.Window;
+            this.nine.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nine.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nine.Location = new System.Drawing.Point(139, 163);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(45, 32);
+            this.nine.TabIndex = 12;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = false;
+            this.nine.Click += new System.EventHandler(this.nine_Click);
+            // 
+            // umnogenie
+            // 
+            this.umnogenie.BackColor = System.Drawing.SystemColors.Window;
+            this.umnogenie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.umnogenie.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.umnogenie.Location = new System.Drawing.Point(202, 163);
+            this.umnogenie.Name = "umnogenie";
+            this.umnogenie.Size = new System.Drawing.Size(45, 32);
+            this.umnogenie.TabIndex = 13;
+            this.umnogenie.Text = "*";
+            this.umnogenie.UseVisualStyleBackColor = false;
+            this.umnogenie.Click += new System.EventHandler(this.umnogenie_Click);
+            // 
+            // nul
+            // 
+            this.nul.BackColor = System.Drawing.SystemColors.Window;
+            this.nul.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nul.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nul.Location = new System.Drawing.Point(16, 213);
+            this.nul.Name = "nul";
+            this.nul.Size = new System.Drawing.Size(45, 32);
+            this.nul.TabIndex = 14;
+            this.nul.Text = "0";
+            this.nul.UseVisualStyleBackColor = false;
+            this.nul.Click += new System.EventHandler(this.nul_Click);
+            // 
+            // ravno
+            // 
+            this.ravno.BackColor = System.Drawing.SystemColors.Window;
+            this.ravno.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ravno.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ravno.Location = new System.Drawing.Point(76, 213);
+            this.ravno.Name = "ravno";
+            this.ravno.Size = new System.Drawing.Size(108, 32);
+            this.ravno.TabIndex = 15;
+            this.ravno.Text = "=";
+            this.ravno.UseVisualStyleBackColor = false;
+            this.ravno.Click += new System.EventHandler(this.ravno_Click);
+            // 
+            // delenie
+            // 
+            this.delenie.BackColor = System.Drawing.SystemColors.Window;
+            this.delenie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.delenie.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.delenie.Location = new System.Drawing.Point(202, 213);
+            this.delenie.Name = "delenie";
+            this.delenie.Size = new System.Drawing.Size(45, 32);
+            this.delenie.TabIndex = 16;
+            this.delenie.Text = "/";
+            this.delenie.UseVisualStyleBackColor = false;
+            this.delenie.Click += new System.EventHandler(this.delenie_Click);
+            // 
+            // ochstit
+            // 
+            this.ochstit.BackColor = System.Drawing.SystemColors.Window;
+            this.ochstit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ochstit.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ochstit.Location = new System.Drawing.Point(280, 61);
+            this.ochstit.Name = "ochstit";
+            this.ochstit.Size = new System.Drawing.Size(45, 32);
+            this.ochstit.TabIndex = 17;
+            this.ochstit.Text = "C";
+            this.ochstit.UseVisualStyleBackColor = false;
+            this.ochstit.Click += new System.EventHandler(this.ochstit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Peru;
+            this.ClientSize = new System.Drawing.Size(333, 297);
+            this.Controls.Add(this.ochstit);
+            this.Controls.Add(this.delenie);
+            this.Controls.Add(this.ravno);
+            this.Controls.Add(this.nul);
+            this.Controls.Add(this.umnogenie);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.chree);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button chree;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button umnogenie;
+        private System.Windows.Forms.Button nul;
+        private System.Windows.Forms.Button ravno;
+        private System.Windows.Forms.Button delenie;
+        private System.Windows.Forms.Button ochstit;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int Flag;
+        public double a,b;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            Flag = 1;
+            label1.Text = "-";
+            a = Convert.ToDouble(tb.Text);
+            tb.Text = "-";
+
+        }
+
+        private void umnogenie_Click(object sender, EventArgs e)
+        {
+            Flag = 2;
+            label1.Text = "*";
+            a = Convert.ToDouble(tb.Text);
+            tb.Text = "*";
+
+        }
+
+        private void delenie_Click(object sender, EventArgs e)
+        {
+            Flag = 3;
+            label1.Text = "/";
+            a = Convert.ToDouble(tb.Text);
+            tb.Text = "/";
+
+        }
+
+        private void ochstit_Click(object sender, EventArgs e)
+        {
+            tb.Text = " ";
+            a = 0;
+            b = 0;
+
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+            tb.Text = tb.Text + "1";
+        }
+
+        private void two_Click(object sender, EventArgs e)
+        {
+            tb.Text = tb.Text + "2";
+        }
+
+        private void chree_Click(object sender, EventArgs e)
+        {
+            tb.Text = tb.Text + "3";
+        }
+
+        private void four_Click(object sender, EventArgs e)
+        {
+            tb.Text = tb.Text + "4";
+        }
+
+        private void five_Click(object sender, EventArgs e)
+        {
+            tb.Text = tb.Text + "5";
+        }
+
+        private void six_Click(object sender, EventArgs e)
+        {
+            tb.Text = tb.Text + "6";
+        }
+
+        private void seven_Click(object sender, EventArgs e)
+        {
+            tb.Text = tb.Text + "7";
+        }
+
+        private void eight_Click(object sender, EventArgs e)
+        {
+            tb.Text = tb.Text + "8";
+        }
+
+        private void nine_Click(object sender, EventArgs e)
+        {
+            tb.Text = tb.Text + "9";
+        }
+
+        private void ravno_Click(object sender, EventArgs e)
+        {
+            b = Convert.ToDouble(tb.Text);
+
+
+            if (Flag == 0)
+            {
+                tb.Text = Convert.ToString(a + b);
+            }
+            if (Flag == 1)
+            {
+                tb.Text = Convert.ToString(a - b);
+            }
+            if (Flag == 2)
+            {
+               tb.Text = Convert.ToString(a * b);
+            }
+            if (Flag == 3)
+            {
+                tb.Text = Convert.ToString(a / b);
+            }
+
+
+        }
+
+        private void nul_Click(object sender, EventArgs e)
+        {
+            tb.Text = tb.Text + "0";
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            Flag = 0;
+            label1.Text = "+";
+            a = Convert.ToDouble(tb.Text);
+            tb.Text = "+";
+
+        }
+    }
+}
